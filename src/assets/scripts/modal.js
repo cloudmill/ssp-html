fd1b42caa6b712e13cf080a3736148925d216a6c
droncheck <droncheck@gmail.com>
2021-06-23 13:47:06 +0300

    правки модальных окон и слайдера с командой

--- src/assets/scripts/modal.js
+++ src/assets/scripts/modal.js
@@ -1,9 +1,22 @@
+const $overlay = document.querySelector('.overlay');
+
+if ($overlay) {
+  $overlay.addEventListener('click', () => {
+    $overlay.classList.remove('active');
+
+    document.querySelectorAll('.modal').forEach($modal => {
+      $modal.classList.remove('active');
+    });
+  });
+}
+
 document.addEventListener('click', (e) => {
   const $target = e.target;
 
   if ($target.dataset.modalBtn) {
     e.preventDefault();
 
+    $overlay.classList.add('active');
     document
         .querySelector(`.modal[data-modal="${$target.dataset.modalBtn}"]`)
         .classList.add('active');
